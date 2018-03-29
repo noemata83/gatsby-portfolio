e80954e3cd8cff37a08e792e0622415075e6c0b0
Tucker McKinney <noemata83@gmail.com>
2018-03-29 01:57:52 -0400

    Add blog post and restyle blog page

--- src/blogs/hello-world.md
+++
@@ -1,6 +0,0 @@
----
-title: "'Sup, world?"
-date: "2018-03-28"
----
-
-Testing: 1, 2, 3.
--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -7,7 +7,7 @@ export default ({ data }) => {
         <div className="center mw8 mt4 pv4 ph2-m">
             <h1>{post.frontmatter.title}</h1>
             <div dangerouslySetInnerHTML={{__html: post.html }} />
-            <Link to="/blog" className="link">Return to blog index</Link>
+            <Link to="/blog" className="db mt5 f4 link">Return to blog index</Link>
         </div>
     )
 }
