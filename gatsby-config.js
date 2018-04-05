c006643d2271e602e32563a07de8892db0548321
Tucker McKinney <noemata83@gmail.com>
2018-04-05 17:28:29 -0400

    Added analytics plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -10,6 +10,15 @@ module.exports = {
         path: `${__dirname}/src`
       }
     },
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: "UA-117045139-1",
+        head: true,
+        anonymize: true,
+        respectDNT: true,
+      },
+    },
     `gatsby-transformer-remark`,
     'gatsby-plugin-react-helmet'
   ],
